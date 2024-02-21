e0e2f349ffd84f98855f84437bbca1536cc4ca5f
Jonwil <wilsonchay201830530@cunoc.edu.gt>
2024-02-20 22:23:42 -0600

    Inciando Algoritmo insercion y Mezcla, Declaracion y abstraccion

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
 
+int calcularPosicion_nDims(int coordenadas[], int tamanos_Dims[]){
+    int posicion = 0;
+    int cantidadDimensiones = sizeof coordenadas/sizeof coordenadas[0];
+
+    for (int i = 0; i < cantidadDimensiones; ++i) {
+        int pos;
+        if(i==0){
+            pos +=  coordenadas[cantidadDimensiones-1];
+        }else{
+            pos =
+
+        }
+
+    }
+
+
+//    i   j    k
+//    i(#j*#k)+(j*#k)+k
+
+
+    return posicion;
+}
+int resultado(){
+
+}
+
+
 int main() {
     int cantidadCordenadas;
     int opcion;
@@ -8,8 +35,8 @@ int main() {
     cout << "Para comenzar, ingrese la cantidad de cordenadas a ingresar" << endl;
     cin >> cantidadCordenadas;
 
-    int coordenadas[cantidadCordenadas];
-    int tamanosDimension[cantidadCordenadas];
+    int  coordenadas[cantidadCordenadas];
+    int  tamanosDimension[cantidadCordenadas];
 
     for (int i = 0; i < cantidadCordenadas; ++i) {
         cout << "Ingrese una cordenada" << endl;
@@ -27,14 +54,13 @@ int main() {
             cout << "Coordenada: " << coor << endl;
         }
         cout << "Tamanos maximos de cada dimension: " << endl;
-        for (int coor: tamanosDimension) {
+        for (int  coor: tamanosDimension) {
             cout << "Tamano de dimension: " << coor << endl;
         }
 
+    int posicioncalculada;
+    posicioncalculada = calcularPosicion_nDims(coordenadas,tamanosDimension);
 
-//    int calcularPosicionEnArreglodeN_Dims(){
-//
-//    }
 
 
 
